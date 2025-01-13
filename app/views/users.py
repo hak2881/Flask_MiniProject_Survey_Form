5372d1965e035cad555f03eba7dc2381e291ba4d
wqeracv <tjdqja1223@gmail.com>
2025-01-13 15:22:05 +0900

    user api 생성

--- app/views/users.py
+++ app/views/users.py
@@ -4,9 +4,13 @@ from flask_smorest import Blueprint
 from config import db
 from models import User
 
-def get():
-    users = User.query.all()
-    return users
+user_blp = Blueprint("Users", "users", description="Operations on users", url_prefix="/users")
+
+@user_blp.route("/")
+class User(MethodView):
+    def get(self):
+        users = User.query.all()
+        return jsonify(users)
 
 def post():
     user_data = request.json
@@ -18,3 +22,27 @@ def post():
         )
     db.session.add(new_user)
     db.session.commit()
+
+@user_blp.route('/<int:user_id>')
+class Users(MethodView):
+    def get(self,user_id):
+        user = User.query.get_or_404(user_id)
+        return {"name": user.name, 'email': user.email}
+    
+    def put(self, user_id):
+        user = User.query.get_or_404(user_id)
+        user_data = request.json
+
+        user.name = user_data['name']
+        user.email = user_data['email']
+        user.gender = user_data['gender']
+        user.age = user_data['age']
+
+        db.session.commit()
+        return {"message": "User updated"}
+
+    def delete(self, user_id):
+        user = User.query.get_or_404(user_id)
+        db.session.delete(user)
+        db.session.commit()
+        return {"message": "User deleted"}
